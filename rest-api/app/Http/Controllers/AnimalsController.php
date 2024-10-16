d5ec013c63b4c1b87c162088298143a93b64377d
Firenze Higa Putra <firenzehiga@gmail.com>
2024-10-16 19:47:12 +0700

    edit

---
+++ rest-api/app/Http/Controllers/AnimalsController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AnimalsController extends Controller
+{
+    //
+
+    public function index()
+    {
+        echo "Menampilkan data animals";
+    }
+    public function store()
+    {
+        echo "Menambahkan data animals";
+    }
+    public function update($id)
+    {
+        echo "Memperbarui data animals";
+    }
+    public function destroy()
+    {
+        echo "Menghapus data animals";
+    }
+}
