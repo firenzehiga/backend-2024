408eee3e0789c1d61600724541851fd29ad48453
Firenze Higa Putra <firenzehiga@gmail.com>
2024-12-11 14:24:31 +0700

    Update task 10

--- tugas10/task.js
+++ tugas10/task.js
@@ -4,7 +4,6 @@
  */
 
 // Producing asynchronous menggunakan Promise
-
 const showDownload = (result) => {
   console.log("Download selesai");
   console.log(`Hasil Download: ${result}`); // Menggunakan ES6 literals
@@ -12,7 +11,7 @@ const showDownload = (result) => {
 
 /**
  * Fungsi untuk download file
- * @param {function} callback - Function callback show
+ * @param {function} Promises
  */
 const download = () => {
   return new Promise((resolve) => {
@@ -23,9 +22,7 @@ const download = () => {
   });
 };
 
-
 // Consuming promise menggunakan Async Await
-
 const main = async () => {
   try {
     console.log("Mendownload selama 3 detik...")
@@ -38,12 +35,3 @@ const main = async () => {
 
 
 main(); // akan dijalankan saat file di node
-
-
-
-/**
- * TODO:
- * - Refactor callback ke Promise atau Async Await
- * - Refactor function ke ES6 Arrow Function
- * - Refactor string ke ES6 Template Literals
- */
